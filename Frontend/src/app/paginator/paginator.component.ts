d9ecd98f9b9b62309d8bfc76a454e427cb76e7a2
eddi <h.amiltone.diel@gmail.com>
2021-04-01 01:43:37 -0500

    agregando los cambios de paginator

--- Frontend/src/app/paginator/paginator.component.ts
+++ Frontend/src/app/paginator/paginator.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'paginator-nav',
   templateUrl: './paginator.component.html'
 })
-export class PaginatorComponent implements OnInit {
+export class PaginatorComponent implements OnInit ,OnChanges {
 
   @Input() paginador : any;
   paginas : number[];
@@ -14,9 +14,21 @@ export class PaginatorComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-    this.paginas = new Array(this.paginador.totalPages).fill(0).map((_valor, indice) => indice + 1);
+  ngOnInit() {
+
+    
+
   }
 
+  ngOnChanges() {
+    this.desde = Math.min(Math.max(1, this.paginador.number-4),this.paginador.totalPages -5);
+    this.hasta = Math.max(Math.min(this.paginador.totalPages, this.paginador.number+4), 6);
+
+    if(this.paginador.totalPages > 5){
+      this.paginas = new Array(this.hasta- this.desde).fill(0).map((_valor, indice) => indice + this.desde);
+    }else{
+      this.paginas = new Array(this.paginador.totalPages).fill(0).map((_valor, indice) => indice + 1);
+    }
+  }
 
 }
